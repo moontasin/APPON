9837a8184169d7615c5c1217cf6b6bfa178965e2
moontasin <me@moontasin.com>
2020-12-27 19:00:18 +0600

    md device

--- js/script.js
+++ js/script.js
@@ -108,3 +108,85 @@ $('#banner').slick({
       // instead of a settings object
     ]
   });
+
+
+  $('.slider').slick({
+    dots: false,
+    fade:true,
+    asNavFor:".img-slider",
+    autoplaySpeed: 2000,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+   arrows:false,
+    responsive: [{
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
+
+
+$('.img-slider').slick({
+    dots: true,
+    centerMode:true,
+    centerPadding:0,
+    autoplay:true,
+    asNavFor: ".slider",
+    autoplaySpeed: 2000,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+   arrows:false,
+    responsive: [{
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint:576,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
